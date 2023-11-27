d786602b153ab95aa4da5d5a7d06f454511da7d5
yogita-s-24 <yogitashete24@gmail.com>
2023-11-27 13:01:46 +0530

    feat: create booking schema for user-bus reservations

---
+++ server/models/Booking.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"User",
+      required: true,
+    },
+  bus: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Bus",
+    required: true,
+  },
+  contactNumber: {
+    type: String,
+    required: true,
+  },
+  seatNumber: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  isConfirmed: {
+    type: Boolean,
+    default: false,
+  },
+  to:{
+    type:String,
+    required:true
+  },
+  from:{
+    type:String,
+    required:true
+  }
+
+},{timestamps:true});
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
