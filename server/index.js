141644968ffdcf6007f82d14ce3a4588e4062a64
yogita-s-24 <yogitashete24@gmail.com>
2023-11-27 14:28:01 +0530

    feat(api): add DELETE endpoint for delete users

--- server/index.js
+++ server/index.js
@@ -8,75 +8,90 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const connectionDB = async() =>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-    
-        if(conn){
-            console.log("MongoDB Connected");
-        }
+const connectionDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
+    if (conn) {
+      console.log("MongoDB Connected");
     }
-    catch(err){
-        console.error("MongoDB is not connected");
-    }
+  } catch (err) {
+    console.error("MongoDB is not connected");
+  }
 };
 connectionDB();
 
-app.get('/api/healths', async(req,res)=>{
-    res.send("Server is running on.")
-})
+app.get("/api/healths", async (req, res) => {
+  res.send("Server is running on.");
+});
 
 //post API - /api/users
 
-app.post('/api/users', async(req,res)=>{
-    const {userName, email, password} = req.body;
+app.post("/api/users", async (req, res) => {
+  const { userName, email, password } = req.body;
 
-    const user = new User({
-        userName: userName,
-        email:email,
-        password:password
-    })
+  const user = new User({
+    userName: userName,
+    email: email,
+    password: password,
+  });
 
-    try{
-        const saveUserData = await user.save();
-    
-        res.json({
-            success:true,
-            data:saveUserData,
-            message:"User Create Successfully"
-        });
-    }catch(err){
-        return res.json({
-            success:false,
-            error: err.message
-            });
-    }
-})
+  try {
+    const saveUserData = await user.save();
+
+    res.json({
+      success: true,
+      data: saveUserData,
+      message: "User Create Successfully",
+    });
+  } catch (err) {
+    return res.json({
+      success: false,
+      error: err.message,
+    });
+  }
+});
 
 //get API - /api/users
 
-app.get('/api/users', async(req, res)=>{
-
-try{
+app.get("/api/users", async (req, res) => {
+  try {
     const alluserData = await User.find();
     res.json({
-        success:true,
-        data : alluserData,
-        message:'All users'
-        })
-}
-catch(err){
+      success: true,
+      data: alluserData,
+      message: "All users",
+    });
+  } catch (err) {
     res.json({
-        success:false,
-        message:'Error in fetching the data'
-        })
-}
+      success: false,
+      message: "Error in fetching the data",
+    });
+  }
 });
 
-const PORT = process.env.PORT || 5000
+//delete API - /api/users/:id
 
+app.delete("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`);
+  try {
+    const deleteUser = await User.deleteOne({ _id: id });
+    res.json({
+      success: true,
+      data: deleteUser,
+      message: "User deleted Successfully.",
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: "User not deleted.",
+    });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
