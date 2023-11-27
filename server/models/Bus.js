390a45ba257ed4dd500379b8e302241711ceadfe
yogita-s-24 <yogitashete24@gmail.com>
2023-11-27 13:02:45 +0530

    feat: create bus schema for managing bus details

---
+++ server/models/Bus.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const busSchema = new mongoose.Schema({
+  busNumber: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  busType:{
+    type :String ,
+    enum : ['AC', 'Non AC'],
+    default:'Non AC',
+    required:true
+  }
+});
+
+const Bus = mongoose.model("busSchema", Bus);
+
+export default Bus;
